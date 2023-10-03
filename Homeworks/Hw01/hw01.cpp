a13d48fc0d141e5cc5b38be9e620dd4a6746a4f1
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-10-03 20:52:43 +0200

    added quality of life functions

--- Homeworks/Hw01/hw01.cpp
+++ Homeworks/Hw01/hw01.cpp
@@ -5,41 +5,51 @@
 #include "hw01.h"
 
 
-
 int main() {
 
-//    inner park must have at least S number of mountains
-//    outer park must have maximal amount of forests
-    park park = read_input();
+//    inner map must have at least S number of mountains
+//    outer map must have maximal amount of forests
+    map map = read_input();
 
-    for (int i = park.inner_park_min_possible; i < park.inner_park_max_possible; ++i) {
-        for (int j = park.inner_park_min_possible; j < park.inner_park_max_possible; ++j) {
-            int mountains_in_row = park.mountains_map_sum[COORD(j,i)];
+//  iterate through the whole map and find optimal map locations
+//  for every possible inner park location
+//  check if it has enough mountains (use partial summs)
+
+    for (int i = 0; i < map.N_whole_area; ++i) {
+        for (int j = 0; j < map.N_whole_area; ++j) {
+//            check mountains here
+            if (is_inner_park(i, j, &map)) {
+
+            }
+
+//            check forests here
         }
     }
 
-
-//    iterate through the whole map and find optimal park locations
-//    for every possible inner park location
-//    check if it has enough mountains (use partial summs)
-
-
-
-
-//      from all parks select ones with the most forests
-//      use partial summs
 
 //      output amount of optimal parks
 
     exit(SUCCESS);
 }
 
+bool is_inner_park(int x, int y, map *map) {
+    bool ret = false;
+    if ((x > map->inner_park_min) && (x < map->inner_park_max) && y > (map->inner_park_min) &&
+        (y < map->inner_park_max)) {
+        ret = true;
+    }
+    return ret;
+}
 
-park read_input(){
+int COORD(int x, int y, int width) {
+    return y * width + x;
+}
+
+map read_input() {
     int N_whole_area, K_outer_park_area, L_inner_park_area_gap, S_minimal_mountain_count;
     std::cin >> N_whole_area >> K_outer_park_area >> L_inner_park_area_gap >> S_minimal_mountain_count;
 
-    park park = {N_whole_area, K_outer_park_area, L_inner_park_area_gap, S_minimal_mountain_count};
+    map park = {N_whole_area, K_outer_park_area, L_inner_park_area_gap, S_minimal_mountain_count};
 
     int forests_summ = 0;
     int mountains_summ = 0;
@@ -61,19 +71,19 @@ park read_input(){
 }
 
 
-void calculate_partial_summs(park *park, char tile, int forests_summ, int mountains_summ) {
+void calculate_partial_summs(map *map, char tile, int forests_summ, int mountains_summ) {
     switch (tile) {
         case Field:
-            park->forests_map_count.push_back(forests_summ);
-            park->mountains_map_sum.push_back(mountains_summ);
+            map->forests_map_count.push_back(forests_summ);
+            map->mountains_map_sum.push_back(mountains_summ);
             break;
         case Forest:
-            park->forests_map_count.push_back(++forests_summ);
-            park->mountains_map_sum.push_back(mountains_summ);
+            map->forests_map_count.push_back(++forests_summ);
+            map->mountains_map_sum.push_back(mountains_summ);
             break;
         case Mountain:
-            park->forests_map_count.push_back(forests_summ);
-            park->mountains_map_sum.push_back(++mountains_summ);
+            map->forests_map_count.push_back(forests_summ);
+            map->mountains_map_sum.push_back(++mountains_summ);
             break;
         default:
             std::cout << "Invalid input" << std::endl;
--- Homeworks/Hw01/hw01.h
+++ Homeworks/Hw01/hw01.h
@@ -11,13 +11,13 @@
 #define SUCCESS 0
 #define ERR_INPUT 101
 
-struct park {
+struct map {
     int N_whole_area{}, K_outer_park_area{}, L_inner_park_area_gap{}, S_minimal_mountain_count{};
     int inner_park_length = K_outer_park_area - 2 * L_inner_park_area_gap;
-    int inner_park_min_possible = L_inner_park_area_gap;
-    int inner_park_max_possible = N_whole_area - L_inner_park_area_gap;
+    int inner_park_min = L_inner_park_area_gap;
+    int inner_park_max = N_whole_area - L_inner_park_area_gap;
 
-    #define COORD(x, y) (x + y * N_whole_area)
+
     #define Field 0
     #define Forest 1
     #define Mountain 2
@@ -27,7 +27,9 @@ struct park {
     std::vector<int> forests_map_count;
 };
 
-park read_input();
-void calculate_partial_summs(park *park, char tile, int forests_summ, int mountains_summ);
+bool is_inner_park(int x, int y, map *map);
+int COORD(int x, int y, int width);
+map read_input();
+void calculate_partial_summs(map *map, char tile, int forests_summ, int mountains_summ);
 
 #endif //HW01_HW01_H
