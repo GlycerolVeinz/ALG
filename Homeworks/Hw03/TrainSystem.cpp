a836594cca923cab5cbc28e772ded3855465d350
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-10-30 19:31:52 +0100

    started work on train stations

---
+++ Homeworks/Hw03/TrainSystem.cpp
@@ -0,0 +1,16 @@
+//
+// Created by glycerolveinz on 30.10.23.
+//
+
+#include "TrainSystem.h"
+#include <iostream>
+
+int main() {
+    int numOfCities;
+    std::cin >> numOfCities;
+    for (int i = 0; i < numOfCities; i++) {
+        int cityNum;
+        std::cin >> cityNum;
+    }
+    return 0;
+}
---
+++ Homeworks/Hw03/TrainSystem.h
@@ -0,0 +1,8 @@
+//
+// Created by glycerolveinz on 30.10.23.
+//
+
+#ifndef HW03_TRAINSYSTEM_H
+#define HW03_TRAINSYSTEM_H
+
+#endif //HW03_TRAINSYSTEM_H
