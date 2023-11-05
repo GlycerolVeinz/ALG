78ebdc3f9631ddf572d17089f20025497aa33ed3
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-11-05 18:54:19 +0100

    did some basic class structure

--- Homeworks/Hw02/Storage.cpp
+++ Homeworks/Hw02/Storage.cpp
@@ -3,3 +3,80 @@
 //
 
 #include "Storage.h"
+
+// ROOM CLASS ==========================================================================================================
+Room::Room(int id, int transportTime, Room *previous) {
+this->Id = id;
+    this->transportTime = transportTime;
+    this->curWeight = 0;
+    this->previous = previous;
+    this->left = nullptr;
+    this->right = nullptr;
+}
+
+int Room::getId() const {
+    return Id;
+}
+
+const std::vector<int> &Room::getBoxes() const {
+    return boxes;
+}
+
+Room *Room::getPrevious() const {
+    return previous;
+}
+
+Room *Room::getLeft() const {
+    return left;
+}
+
+Room *Room::getRight() const {
+    return right;
+}
+
+int Room::getTransportTime() const {
+    return transportTime;
+}
+
+int Room::getCurWeight() const {
+    return curWeight;
+}
+
+void Room::setId(int id) {
+    Id = id;
+}
+
+
+void Room::setPrevious(Room *parent) {
+    Room::previous = parent;
+}
+
+void Room::setLeft(Room *leftRoom) {
+    Room::left = leftRoom;
+}
+
+void Room::setRight(Room *rightRoom) {
+    Room::right = rightRoom;
+}
+
+void Room::setTransportTime(int t) {
+    Room::transportTime = t;
+}
+
+void Room::setCurWeight(int cW) {
+    Room::curWeight = cW;
+}
+
+
+// STORAGE CLASS =======================================================================================================
+Storage::Storage(Room mainRoom) {
+    this->mainRoom = &mainRoom;
+}
+
+Room *Storage::getMainRoom() const {
+    return mainRoom;
+}
+
+void Storage::setMainRoom(Room *mainRoom) {
+    Storage::mainRoom = mainRoom;
+}
--- Homeworks/Hw02/Storage.h
+++ Homeworks/Hw02/Storage.h
@@ -5,9 +5,56 @@
 #ifndef HW02_STORAGE_H
 #define HW02_STORAGE_H
 
+#include <vector>
+
+class Room;
 
 class Storage {
+private:
+    Room *mainRoom;
 
+    int totalWeight;
+
+public:
+    Storage(Room mainRoom);
+
+    Room *getMainRoom() const;
+
+    void setMainRoom(Room *mainRoom);
+};
+
+class Room {
+private:
+    int Id;
+
+    std::vector<int> boxes;
+
+    Room *previous;
+    Room *left;
+    Room *right;
+
+    int transportTime;
+    int curWeight;
+
+public:
+    Room(int id, int transportTime, Room *previous);
+
+    int getId() const;
+
+    const std::vector<int> &getBoxes() const;
+
+    Room *getPrevious() const;
+    Room *getLeft() const;
+    Room *getRight() const;
+    int getTransportTime() const;
+    int getCurWeight() const;
+
+    void setId(int id);
+    void setPrevious(Room *parent);
+    void setLeft(Room *leftRoom);
+    void setRight(Room *rightRoom);
+    void setTransportTime(int t);
+    void setCurWeight(int cW);
 };
 
 
